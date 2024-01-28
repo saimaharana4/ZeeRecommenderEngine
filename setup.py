fc297347dea05c44d14ac8b102b7a775e50db4e3
SAI PRASANNA MAHARANA <60200140+saimaharana4@users.noreply.github.com>
2024-01-28 14:23:18 +0530

    added initiatl commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Witha a given dataset, we are recommending a movie based on past data and we named as ZeeRecommenderSyatem',
+    author='Sai',
+    license='',
+)
